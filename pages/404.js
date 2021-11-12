b4f68d605debbcfff8948629d2b5f0eae704345a
marina.anisimova <marina.anisimova@itrexgroup.com>
2021-11-12 16:10:00 +0300

    complete tutorial

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Layout from "../components/layout/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>404 - Page Not Found</h1>
+    </Layout>
+  );
+}
